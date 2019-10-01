caf79c2d07cd3d2f86f91accb4271f44e91cd6c4
Axel Savolainen <axelsavolainen@kamk.fi>
2019-10-01 10:39:32 +0300

    Added a working server code

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 // Tänne tulee palvelimen koodi
+const express = require('express');
+
+const app = express();
+const port = 8080;
+const host = '0.0.0.0';
+
+app.get('/', function(req, res) {
+    res.send("Moi maailma!");
+});
+
+app.listen(port, host, () => {
+  console.log("server up and running at http://" + host + ":" + port);
+});
+
+module.exports = app; // for TDD
